c1de9d3fb61189190ee6ad9033df15a29d74c68a
heitorgentil <heitor_albuquerque@yahoo.com.br>
2026-07-25 11:08:27 -0300

    feat: add Supabase configuration to initialize client

---
+++ supabase.config.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://pizrfntxmiqekkvayoxr.supabase.co'; // Replace with your Supabase URL
+const supabaseKey = 'sb_publishable_3dPNfKx16yelUgKiXc783w_5aTpueNa';
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
